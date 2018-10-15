6a4095f4eb9f53095c457e32e6f1d394f1365b06
Alexstericris <42732998+Alexstericris@users.noreply.github.com>
2018-10-15 12:38:03 +0200

    Create Gui.java

---
+++ Gui.java
@@ -0,0 +1,28 @@
+
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.util.*;
+
+public class TextManagerGUI extends JFrame {
+
+	public TextManagerGUI(){
+		
+		super("Text Manager");
+		setLayout(new FlowLayout());
+		this.setVisible(true);
+		this.setSize(800, 600);
+		
+		//Make Normal Window Extended
+		super.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+}
